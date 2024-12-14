ef90441091fbf15eb51d8a53bcd9df6536822516
schrodingerscat1 <cameronwilliamchu@gmail.com>
2024-12-14 16:28:43 -0500

    Receive computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+ // console.log("Hello World")
+
+let x = Math.floor(Math.random() * 3);
+
+console.log(x);
+
+function getComputerChoice(){
+  
+    if (x===0){
+        computerChoice = "Rock";
+        console.log("Rock");
+    }
+    else if (x===1){
+        computerChoice = "Paper";
+        console.log("Paper");
+    }
+    else if (x===2){
+        computerChoice = "Scissors";
+        console.log("Scissors");
+    }
+    
+    return 0;
+}
+
+getComputerChoice();
+
+
+
+ 
+
